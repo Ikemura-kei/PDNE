333437872de79fac3f9717eb4a32bdbde5d57e88
lastbasket <c1135949168@gmail.com>
2023-11-03 21:30:26 +0800

    added late fusion

---
+++ model/completionformer_late_fusion/completionformer_late_fusion.py
@@ -0,0 +1,57 @@
+from .completionformer import CompletionFormer
+import torch
+import torch.nn as nn
+from .backbone_late_fusion import BackboneLateFusion
+from .backbone_late_fusion import conv_bn_relu
+
+class CompletionFormerLateFusion(nn.Module):
+    def __init__(self, args):
+        super(CompletionFormerLateFusion, self).__init__()
+
+        self.args = args
+
+        self.foundation = CompletionFormer(args)
+        self.foundation.load_state_dict(torch.load(args.pretrained_completionformer, map_location='cpu')['net'])
+
+        self.prop_time = self.args.prop_time
+        self.num_neighbors = self.args.prop_kernel*self.args.prop_kernel - 1
+        self.backbone = BackboneLateFusion(args, self.foundation.backbone, mode='rgbd')
+
+        self.conv_pol = conv_bn_relu(4, 4, kernel=1, stride=1, padding=0,
+                                          bn=False)
+
+        if self.prop_time > 0:
+            self.prop_layer = self.foundation.prop_layer
+    
+    def forward(self, sample):
+        rgb = sample['rgb']
+        dep = sample['dep']
+        pol = sample['pol']
+
+        pred_init, guide, confidence = self.backbone(rgb, dep, pol)
+        pred_init = pred_init + dep
+        
+        # -- difussion --
+        y_inter = [pred_init, ]
+        conf_inter = [confidence, ]
+        if self.prop_time > 0:
+            y, y_inter, offset, aff, aff_const = \
+                self.prop_layer(pred_init, guide, confidence, dep, rgb)
+        else:
+            y = pred_init
+            offset, aff, aff_const = torch.zeros_like(y), torch.zeros_like(y), torch.zeros_like(y).mean()
+
+        # Remove negative depth
+        y = torch.clamp(y, min=0)
+        
+        # best at first
+        y_inter.reverse()
+        conf_inter.reverse()
+        if not self.args.conf_prop:
+            conf_inter = None
+
+        output = {'pred': y, 'pred_init': pred_init, 'pred_inter': y_inter,
+                  'guidance': guide, 'offset': offset, 'aff': aff,
+                  'gamma': aff_const, 'confidence': conf_inter}
+
+        return output
